daefa8261fcf0e9d522ade4e30653ea673f66f48
swilliams29 <swillia47@rockets.utoledo.edu>
2020-05-10 15:34:55 -0500

    Add project files.

---
+++ TabbedPagesExamples/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TabbedPagesExamples.Models;
+
+namespace TabbedPagesExamples.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
